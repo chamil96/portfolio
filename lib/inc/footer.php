23ae80b898dbe23cabf07e3aee5f13004fc126b2
Chris Hamilton <chrishamilton@Administrators-MacBook-Pro.local>
2017-10-18 15:01:59 -0400

    Adjusted headers

--- lib/inc/footer.php
+++ lib/inc/footer.php
@@ -1,6 +1,6 @@
 <div class="bottom_wrapper row">
   <footer class="footer">
-    <h6>&copy; Chris Hamilton 2017 | </h6>
+    <h6>&copy; Chris Hamilton 2017</h6>
     <div class="social_media row">
       <a href="https://www.linkedin.com/in/christopher-hamilton-26b4b4146/"><img class="sm_icons" src="lib/img/linkedinlogo.png" alt="linkedin"></a>
       <a href="https://www.facebook.com/chris.hamilton.18400700"><img class="sm_icons" src="lib/img/facebookicon.png" alt="facebook"></a>
