fd60646bf934f218c0034cce8402eb9965e69731
Chris Hamilton <chrishamilton@Administrators-MacBook-Pro.local>
2017-10-18 14:39:16 -0400

    Upadated resume

--- resume.php
+++ resume.php
@@ -2,36 +2,76 @@
   include ("lib/inc/header.php")
 ?>
   <div class="resume_content row">
-    <h1 id="resume_header">Resume</h1>
-    <article class="resume">
-      <h2>Work Experience:</h2>
-      <p><b>Ruth’s Chris Steakhouse, Charlotte, NC</b></p>
-      <p>Server’s Assistant
-        Nov 2016 – July 2017
-        Multitasked in a high volume environment  while engaged in outstanding client service.</p>
-      <p><b>Charlotte Douglas Airport, Charlotte, NC</b></p>
-      <p>Customs Baggage Handler
-        Sept 2015 – Nov 2016
-        Maintained level of security to Federal Inspection Area and provided clients with personal property in quick time.</p>
+    <div class="resume">
+      <h1 id="resume_header">Resume</h1>
+      <header>
+    		<h2 id="resume_subheader">Christopher Hamilton</h2>
+    		<ul title="mail and phone">
+    			<li>Charlotte, NC</li>
+    			<li>(704)-763-2062</li>
+    		</ul>
+    		<ul id="header-right" title="web">
+    			<li>naseemhamilton96@gmail.com</li>
+    			<li>LinkedIn</li>
+    		</ul>
+    	</header>
+    	<section><h3 id="resumeh3">Education:</h3>
+    		<ul title="education">
+          <li>
+      			<h4>Road to Hire, Fort Mill, SC</h4>
+              	<ul>
+              		<li>Road to Hire Participant
+                    June 2017- October 2017</li>
+             		</ul>
+          </li>
+    		<li>
+    			<h4>University of North Carolina at Charlotte, Charlotte, NC</h4>
+            	<ul>
+            		<li>Aug 2014- May 2015</li>
+           		</ul>
+        </li>
+        <li>
+    			<h4>Phillip O. Berry Academy Technology, Charlotte, NC</h4>
+            	<ul>
+            		<li>Aug 2010- June 2014</li>
+                <li>Graduated from the engineering academy with a 3.8 weighted GPA</li>
+           		</ul>
+        </li>
+    		</ul>
+    	</section>
+    	<section><h3 id="resumeh3">Skills:</h3>
+    		<ul title="skills">
+    			<li>Git</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
+          <li>PHP</li>
+          <li>public speaking</li>
+    		</ul>
+    	</section>
+    	<section><h3 id="resumeh3">Experience:</h3>
+    	<ul title="experience">
+    		<li>
+    			<h4>Ruth’s Chris Steakhouse, Charlotte, NC</h4>
+    			<ul>
+    				<li>Server’s Assistant
+              Nov 2016 – July 2017
+              Multitasked in a high volume environment  while engaged in outstanding client service.</li>
 
-      <h2>Education:</h2>
-      <p><b>Road to Hire, Fort Mill, SC</b></p>
-      <p>Road to Hire Participant
-        June 2017- Present</p>
-      <p><b>University of North Carolina at Charlotte, Charlotte, NC</b></p>
-      <p>Aug 2014- May 2015</p>
-      <p><b>Phillip O. Berry Academy Technology, Charlotte, NC</b></p>
-      <p>Aug 2010- June 2014
-        Graduated from the engineering academy with a 3.8 weighted GPA</p>
-
-      <h2>Leadership:</h2>
-      <p><b>Student Government Association Freshman Class, Student Representative</b></p>
-      <p>Sept 2014- May 2015</p>
-
-      <h2>Technical Skills:</h2>
-      <p>Skills learned: Git,  HTML, CSS, JavaScript, jQuery, PHP, public speaking.</p>
-
-    </article>
+    			</ul>
+    		</li>
+        <li>
+    			<h4>Charlotte Douglas Airport, Charlotte, NC</h4>
+    			<ul>
+    				<li>Customs Baggage Handler</li>
+            <li>Sept 2015 – Nov 2016</li>
+            <li>Maintained level of security to Federal Inspection Area and provided clients with personal property in quick time.</li>
+    			</ul>
+    		</li>
+    	</ul>
+    	</section>
+    </div>
   </div>
 <?php
   include ("lib/inc/footer.php");
