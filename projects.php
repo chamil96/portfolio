d2cc7b969995561dd49b63ad73692082876c6f21
Chris Hamilton <chrishamilton@Administrators-MacBook-Pro.local>
2017-10-24 16:49:07 -0400

    Update

--- projects.php
+++ projects.php
@@ -5,22 +5,19 @@
     <h1 id="project_header">Projects</h1>
     <div class="project_wrapper">
       <div class="projects">
-        <h2>Guessing Game using React.js</h2>
+        <a href="http://chamilton.road2hire.ninja/Guessgameapp/"><h2>Guessing Game using React.js</h2></a>
       </div>
       <div class="projects">
-        <h2>Flashcard Translator using React.js</h2>
+        <a href="http://chamilton.road2hire.ninja/Flashcardsapp/"><h2>Flashcard Translator using React.js</h2></a>
       </div>
       <div class="projects">
-        <h2>Team Assigner using React.js</h2>
+        <a href="http://chamilton.road2hire.ninja/Teambuilderapp/"><h2>Team Assigner using React.js</h2></a>
       </div>
       <div class="projects">
-        <h2>Chatroom-app Demo using React.js</h2>
+        <a href="http://chamilton.road2hire.ninja/Chatroomapp/"><h2>Chatroom-app Demo using React.js</h2></a>
       </div>
       <div class="projects">
-        <h2>Sample E-Commerce using PHP and MySQL</h2>
-      </div>
-      <div class="projects">
-        <h2>Tabata Timer built using jQuery</h2>
+        <a href="http://chamilton.road2hire.ninja/ecommerce/e-commerce2k17/"><h2>Sample E-Commerce using PHP and MySQL</h2></a>
       </div>
     </div>
   </div>
