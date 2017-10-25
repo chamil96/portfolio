a8546717bc0599f91a7a3f21da57de9cd10580c8
Chris Hamilton <chrishamilton@adminisatorsmbp.attlocal.net>
2017-10-25 11:18:18 -0400

    media query

--- index.php
+++ index.php
@@ -3,15 +3,19 @@
 ?>
   <div class="index_main_content row">
     <div class="wrapper">
-      <header class="banner one-half">
-        <h1 id="header"><span id="nametag">Chris Hamilton:</span> An Aspiring Front-end Developer</h1>
-        <h2>Welcome to my site!</h2>
-      </header>
-      <article class="indexicon one-half">
-        <figure>
-          <img id="laptop" src="lib/img/laptopicon3.png" alt="laptop icon">
-        </figure>
-      </article>
+      <div class="banner one-half">
+        <header>
+          <h1 id="header"><span id="nametag">Chris Hamilton:</span> An Aspiring Front-end Developer</h1>
+          <h2>Welcome to my site!</h2>
+        </header>
+      </div>
+      <div class="indexicon one-half">
+        <article>
+          <figure>
+            <img id="laptop" src="lib/img/laptopicon3.png" alt="laptop icon">
+          </figure>
+        </article>
+      </div>
     </div>
   </div>
 <?php
